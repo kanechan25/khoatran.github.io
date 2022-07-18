e1861b26d1cddf8062a5060f7e419f1f62a1ad24
kanechan25 <kanechan.civilengineer@gmail.com>
2022-07-18 15:53:00 +0700

    edit-menu

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavMenu from './components/NavMenu/NavMenu';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <NavMenu />
+            </header>
+        </div>
+    );
+}
+
+export default App;
